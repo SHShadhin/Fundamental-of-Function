def1b0b90171182d8cff283c0ef358ed01d0f09a
SHShadhin <princeshadhin70@gmail.com>
2026-01-31 21:26:49 +0600

    function structure

---
+++ function.js
@@ -0,0 +1,14 @@
+function webDeveloper () {
+    console.log('html,css')
+    console.log('Javascript')
+    console.log('React.js')
+}
+
+webDeveloper();
+
+// structure
+
+// function #name() {
+//      
+// }
+// #name();
